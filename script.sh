200a7b4ff3b86801ac88303bd3896b49d599f5cd
barraganubc <barraganubc@gmail.com>
2022-07-27 11:46:32 -0500

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+
+echo "Creando archivo"
+
+echo "Evaluacion NPB”" > /home/prueba.txt
+
+
+sudo chmod +r /home/prueba.txt
+
+$ ls -l /home/prueba.txt
+
+cat /home/prueba.txt
+
+$ mkdir /home/tmp
+
+mv /home/prueba.txt /home/tmp/prueba.txt
+
+echo "Hola" >> /home/tmp/prueba.txt
+
+ls -l /home/tmp/
+
+#rm -rf /home/tmp
+
+grep -i "Hola" /home/sprueba.txt
